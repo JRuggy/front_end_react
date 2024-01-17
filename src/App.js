5aa41bd58669981527461428d22d9a02c1fd776a
JRuggy <jastonruggy@icloud.com>
2024-01-17 17:04:45 +0300

    can call objects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Books from './pages/Books'
+import Update from './pages/Update'
+import Add from './pages/Add';
+
 function App() {
   return (
     <div className="App">
-      Jaston Ruggy
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books/>} />
+          <Route path="/add" element={<Add/>} />
+          <Route path="/update" element={<Update/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
